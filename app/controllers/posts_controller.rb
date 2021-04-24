12e178a3fcc9bdbd753c66f4dfd5febfa53f6c12
Christerle Nounoute <christerlesn@gmail.com>
2021-04-24 19:43:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.assign_attributes(post_params)
+    @post = Post.find(params[:id])
     if @post.valid?
-      @post.save
-
+      @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
